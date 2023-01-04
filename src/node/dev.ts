a272944dcddd511766988dd0f1662040be7cf6e6
张康 <zhangkang@zhangkangs-MacBook-Pro.local>
2023-01-04 18:17:49 +0800

    feat: 配置文件解析

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -2,8 +2,12 @@ import { PACKAGE_ROOT } from "./constants/index";
 import { createServer as createViteDevServer } from "vite";
 import { pluginIndexHtml } from "./plugin-island/indexHtml";
 import pluginReact from "@vitejs/plugin-react";
+import { resolveConfig } from "./config";
 
 export async function createDevServer(root = process.cwd()) {
+  const config = await resolveConfig(root, "serve", "development");
+  console.log(config);
+
   return createViteDevServer({
     root,
     plugins: [pluginIndexHtml(), pluginReact()],
