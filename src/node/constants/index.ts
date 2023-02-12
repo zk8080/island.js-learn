b5f02d1c6f5f91744973bac9599636c60001df50
zk8080 <zhangkang8080@163.com>
2023-02-12 21:52:23 +0800

    chore: lint-staged配置优化

--- src/node/constants/index.ts
+++ src/node/constants/index.ts
@@ -4,16 +4,6 @@ export const PACKAGE_ROOT = join(__dirname, "..");
 
 export const DEFAULT_HTML_PATH = join(PACKAGE_ROOT, "template.html");
 
-export const CLIENT_ENTRY_PATH = join(
-  PACKAGE_ROOT,
-  "src",
-  "runtime",
-  "client-entry.tsx"
-);
+export const CLIENT_ENTRY_PATH = join(PACKAGE_ROOT, "src", "runtime", "client-entry.tsx");
 
-export const SERVER_ENTRY_PATH = join(
-  PACKAGE_ROOT,
-  "src",
-  "runtime",
-  "server-entry.tsx"
-);
+export const SERVER_ENTRY_PATH = join(PACKAGE_ROOT, "src", "runtime", "server-entry.tsx");
